96ca2d52e6039847fc6f21397c7980246227bd26
Orkhan <2017orxanaxundov@gmail.com>
2025-08-20 15:10:24 +0400

    Made animations of the main fragment appearance and animations of transitions between other fragments

--- app/src/main/java/com/example/projectwork_1/FavoritesFragment.kt
+++ app/src/main/java/com/example/projectwork_1/FavoritesFragment.kt
@@ -1,10 +1,15 @@
 package com.example.projectwork_1
 
 import android.os.Bundle
+import androidx.transition.Fade
+import androidx.transition.Slide
+import androidx.transition.TransitionSet
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -12,6 +17,24 @@ import androidx.recyclerview.widget.RecyclerView
 class FavoritesFragment : Fragment() {
 
     private val favoritesDataBase = FilmsDatabase.favoriteFilms
+
+    init {
+        enterTransition = Fade(Fade.MODE_IN).apply {
+            duration = 800
+            propagation = null
+        }
+
+        returnTransition = Fade(Fade.MODE_OUT).apply {
+            duration = 800
+            propagation = null
+        }
+
+        exitTransition = Fade(Fade.MODE_OUT).apply {
+            duration = 800
+            propagation = null
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +51,7 @@ class FavoritesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        postponeEnterTransition()
 
         val favoritesRecycler = view.findViewById<RecyclerView>(R.id.favorites_recycler_view)
 
@@ -45,6 +69,7 @@ class FavoritesFragment : Fragment() {
         favoritesRecycler.adapter = adapter
         favoritesRecycler.layoutManager = LinearLayoutManager(requireContext())
         favoritesRecycler.addItemDecoration(HomeFragment.FilmListItemDecor(8))
+        startPostponedEnterTransition()
 
 
     }

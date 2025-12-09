a1610f07a6be1631ebc827b63ae2b4e135370dcb
Orkhan <2017orxanaxundov@gmail.com>
2025-12-09 22:05:54 +0400

    Created a Broadcast Receiver that listens to system messages about battery status and implemented certain logic accordingly

--- app/src/main/java/com/example/projectwork_1/App.kt
+++ app/src/main/java/com/example/projectwork_1/App.kt
@@ -1,7 +1,13 @@
 package com.example.projectwork_1
 
 import android.app.Application
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
 import com.example.domain_api.retrofit.TmdbApi
 import com.example.domain_room_api.db.FilmDao
 import com.example.projectwork_1.data.di.AppComponent
@@ -20,13 +26,15 @@ class App : Application() {
     @Inject
     lateinit var filmDao: FilmDao
 
+    lateinit var receiver: MyChargerBroadcastReceiver
+
     override fun onCreate() {
         super.onCreate()
         instance = this
         getApp().inject(this)
 
         //этот код нам нужен для того, чтобы загружать последнюю выбранную тему, то есть перед выходом из приложения, допустим
-        //что я выбрал темную схему, это сохранилось в sharedPreferences, теперь при следующим запуском приложения, приложение
+        //что я выбрал темную тему, это сохранилось в sharedPreferences, теперь при следующим запуском приложения, приложение
         //будет знать какую тему использовать, а пишем мы это в Арр классе, потому что он создается до создания активити и фрагментов
         //получаем последнюю тему
         val theme = preferenceProvider.getTheme()
@@ -36,6 +44,19 @@ class App : Application() {
         } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
+
+
+        receiver = MyChargerBroadcastReceiver()
+        val intentFilter = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_BATTERY_LOW)
+        }
+
+        //так как класс App живет на протяжении всего приложения, то дерегистрировать receiver не надо
+        this.registerReceiver(
+            receiver,
+            intentFilter
+        )
     }
 
     fun getApp(): AppComponent {
@@ -49,5 +70,26 @@ class App : Application() {
             private set
 
         private var appComponent: AppComponent? = null
+    }
+
+    //Решил BroadcastReceiver сделать с контекстом всего приложения
+    inner class MyChargerBroadcastReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when(intent?.action) {
+                Intent.ACTION_POWER_CONNECTED ->  {
+                    if (preferenceProvider.getTheme() == "dark") {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                        preferenceProvider.saveTheme("light")
+                    }
+                }
+
+                Intent.ACTION_BATTERY_LOW -> {
+                    if (preferenceProvider.getTheme() == "light") {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                        preferenceProvider.saveTheme("dark")
+                    }
+                }
+            }
+        }
     }
 }
